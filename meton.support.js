4fddfc431046f3a39503e7a8a0866e53631508c0
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-14 18:16:41 +0800

    Updated client test.

--- meton.support.js
+++ meton.support.js
@@ -35,6 +35,10 @@
               			"module": "meton",
               			"author": "Richeve S. Bebedor",
               			"eMail": "richeve.bebedor@gmail.com",
+              			"contributors": [
+              				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+              				"Vinse Vinalon"
+              			],
               			"repository": "https://github.com/volkovasystems/meton.git",
               			"test": "meton-test.js",
               			"global": true
